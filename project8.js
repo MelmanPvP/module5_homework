28e538c5ea4132ef46101627f3688e15d6aaff67
Dima <dtsarev05@mail.com>
2024-02-11 17:41:12 +0300

    Update project8.js

--- project8.js
+++ project8.js
@@ -2,4 +2,11 @@ let map = new Map();
 map.set ('Name','Dima');
 map.set('Age', 18);
 map.set('Region', 'Moscow' );
-console.log()
+for (name of map.keys()) {
+    console.log(`Ключ- ${name}`, )
+}
+for (let color  of map.values()) {
+
+    console.log(`Значение- ${color}`);
+
+}
